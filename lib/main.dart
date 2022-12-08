24d542f24a9a508e8067029ded0d85e16670d372
Amirahmad Adibi <amirahmadadibi@Amirahmads-MacBook-Pro.local>
2022-12-08 19:36:19 +0330

    week 14-2 E07 animated switcher

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:animations/opacity_animation.dart';
 import 'package:animations/position_animation.dart';
 import 'package:animations/rotation_animation.dart';
 import 'package:animations/scale_animation.dart';
+import 'package:animations/switch.dart';
 import 'package:animations/text_animation.dart';
 import 'package:flutter/material.dart';
 
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: TextAnimation(),
+      home: AnimationSwitch(),
     );
   }
 }
